f2c9624b0f5ef4579d2a2537512c38301b356649
Vijaykumar Shanmugam <vijay.psg587@gmail.com>
2017-09-15 23:21:29 -0400

    intial commit

---
+++ src/main/java/com/vijay/EurekaServer/App.java
@@ -0,0 +1,19 @@
+package com.vijay.EurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class App 
+{
+    public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+	}
+
+}
