f785c5d963a4cd23f53f74b1a9573ab15c2925e4
Vijaykumar Shanmugam <vijay.psg587@gmail.com>
2017-09-17 00:13:46 -0400

    Modified files for eurekaServer

--- src/main/java/com/vijay/EurekaServer/App.java
+++ src/main/java/com/vijay/EurekaServer/App.java
@@ -2,6 +2,8 @@ package com.vijay.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -11,7 +13,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class App 
-{
+{	
+	
+	
     public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
